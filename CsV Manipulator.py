32fcf1d72232690b58022371d2e20d09c8b2f5ec
Sachin Diwakar <37329968+sachin3339@users.noreply.github.com>
2019-03-12 01:48:41 -0700

    completed the MyTable Function

--- CsV Manipulator.py
+++ CsV Manipulator.py
@@ -12,3 +12,21 @@ from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow, QTableWidge
 from PyQt5.QtWidgets import qApp, QAction
 from mpl_qtwidgets import Form 
 
+class MyTable(QTableWidget):
+    def __init__(self, r, c):
+        super().__init__(r, c)
+        self.check_change = True
+        self.init_ui()
+
+    def init_ui(self):
+        self.cellChanged.connect(self.c_current)
+        self.show()
+
+    def c_current(self):
+        if self.check_change:
+            row = self.currentRow()
+            col = self.currentColumn()
+            value = self.item(row, col)
+            value = value.text()
+            print("The current cell is ", row, ", ", col)
+            print("In this cell we have: ", value)
