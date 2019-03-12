4634dab8920849209a4662fada824b0562338b2a
Sachin Diwakar <37329968+sachin3339@users.noreply.github.com>
2019-03-12 04:55:07 -0700

    Successfully Tested the Spreadsheet program along with UI

--- CsV Manipulator.py
+++ CsV Manipulator.py
@@ -1,32 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 10 09:32:54 2019
 
-@author: fearlesssachin
-"""
 import sys
-import os
-import csv
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow, QTableWidgetItem, QFileDialog,QPushButton
-from PyQt5.QtWidgets import qApp, QAction
-from mpl_qtwidgets import Form 
+from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow
+
 
 class MyTable(QTableWidget):
     def __init__(self, r, c):
         super().__init__(r, c)
-        self.check_change = True
-        self.init_ui()
 
-    def init_ui(self):
-        self.cellChanged.connect(self.c_current)
         self.show()
 
-    def c_current(self):
-        if self.check_change:
-            row = self.currentRow()
-            col = self.currentColumn()
-            value = self.item(row, col)
-            value = value.text()
-            print("The current cell is ", row, ", ", col)
-            print("In this cell we have: ", value)
+
+class Sheet(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.form_widget = MyTable(10, 10)
+        self.setCentralWidget(self.form_widget)
+
+        self.show()
+
+app = QApplication(sys.argv)
+sheet = Sheet()
+sys.exit(app.exec_())
