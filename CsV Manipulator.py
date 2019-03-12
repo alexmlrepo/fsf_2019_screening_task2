3aab2ad6faeaf7ca696f5addfa4482c72101287a
Sachin Diwakar <37329968+sachin3339@users.noreply.github.com>
2019-03-12 01:44:09 -0700

    made the py file in Spyder

---
+++ CsV Manipulator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 09:32:54 2019
+
+@author: fearlesssachin
+"""
+import sys
+import os
+import csv
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow, QTableWidgetItem, QFileDialog,QPushButton
+from PyQt5.QtWidgets import qApp, QAction
+from mpl_qtwidgets import Form 
+
